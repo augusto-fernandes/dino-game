0c3fa176d0eb36ace6d6e97c03a31cdb5b58fe89
Augusto Fernandes Costa <augusto.fcosta1808@gmail.com>
2022-05-23 21:05:18 -0300

    evitando que o dinossauro pule diversas vezes durante a animação

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const dino = document.querySelector(".dino");
+let isJumping = false;
+
 function handleKeyUp(event) {
   //32 é o keycode do espaço
   //site para ver outros key code: https://www.toptal.com/developers/keycode
   if (event.keyCode === 32) {
-    jump();
+      if(!isJumping){
+        jump();
+      }
+    
   }
 }
 
 function jump() {
   let position = 0;
+
+  isJumping=true;
 
   let upInterval = setInterval(() => {
     if (position >= 150) {
@@ -18,6 +25,7 @@ function jump() {
       let downInterval = setInterval(() =>{
           if(position <= 0){
               clearInterval(downInterval);
+              isJumping = false;
           }else{
             position-= 20;
             dino.style.bottom = position + "px";
